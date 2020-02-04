304297a06d2e637a411b3804681cec87ed04f7cb
Andrey Sokolov <andreysokolov@qantas.com.au>
2020-02-04 11:37:37 +1100

    works at last

--- test.cpp
+++ test.cpp
@@ -5,31 +5,42 @@
 
 std::vector<std::string> split(std::string line);
 
+class visitor {
+public:
+    visitor() = default;
+    visitor(std::map<std::string, int> index_map) : index_map_(index_map){}
+    void apply(std::string field, int& i) {
+        i = boost::lexical_cast<int>( elems_[index_map_[field]] );
+    }
+    void set_elems(std::vector<std::string> elems) { elems_ = elems; }
+private:
+    std::map<std::string, int> index_map_;
+    std::vector<std::string> elems_;
+};
+
 template< typename T > class stream {
 public:
     stream(std::string fields) : fields(fields) {
         fields_array = split(fields);
         for(int i=0; i<fields_array.size(); ++i) {
-            if (fields_array[i]=="x") { fields_map["x"] = i; }
-            if (fields_array[i]=="y") { fields_map["y"] = i; }
+            fields_map[fields_array[i]] = i;
         }
+        visitor_ = visitor(fields_map);
     }
     T read() {
         std::string line;
         getline(std::cin, line);
         std::vector<std::string> elems = split(line);
-        result_ = default_;
         T p;
-        lexical_cast_( p.x, elems[fields_map["x"]] );
-        lexical_cast_( p.y, elems[fields_map["y"]] );
+        visitor_.set_elems(elems);
+        visit(p, visitor_);
         return p;
     };
 private:
     std::string fields;
     std::vector<std::string> fields_array;
     std::map<std::string, int> fields_map;
-    T default_ = T();
-    T result_;
+    visitor visitor_;
 
     static void lexical_cast_( int& v, const std::string& s ) {
         v = boost::lexical_cast< int >( s );
@@ -72,15 +83,16 @@ template<> struct traits<Q> {
     template<typename V> static void visit(Q& q, V& v) {
         v.apply("a", q.a);
         v.apply("b", q.b);
-        v.apply("p", q.p);
+//        v.apply("p", q.p);
     }
 };
 
 
-std::string fields = "cat,x,,dog,y,,"; // fields for P
+//std::string fields = "cat,x,,dog,y,,"; // fields for P
 //std::string fields = "a,p/x,b,p/y"; // fields for Q
 
-int main() {
+int main(int argc, char* argv[]) {
+    std::string fields = argv[1];
     stream<P> s(fields);
     while(std::cin) {
         P p = s.read();
