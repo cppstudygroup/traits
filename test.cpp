ca81d25dcc3c9837e3119c95b22d9bb849e48099
Andrey Sokolov <andsok75@fastmail.com>
2020-02-09 21:10:02 +1100

    nested structs (c++17)

--- test.cpp
+++ test.cpp
@@ -1,19 +1,39 @@
-#include <iostream>
 #include <boost/lexical_cast.hpp>
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/algorithm/string/erase.hpp>
+#include <iostream>
 #include <vector>
 #include <map>
 #include <iomanip>
+#include <type_traits>
 
 std::vector<std::string> split(std::string line);
+template<typename T> struct Traits;
 
 class Visitor {
 public:
     Visitor() = default;
-    Visitor(std::map<std::string, unsigned int> index_map) : index_map_(index_map){}
+    Visitor(std::map<std::string, unsigned int> index_map) : index_map_(index_map) {}
     template<typename Property> void apply(std::string field, Property& property) const {
-        auto index = index_map_.find(field);
-        if(index == index_map_.end()) { return; }
-        property = boost::lexical_cast<Property>(elems_[index->second]);
+        if constexpr(std::is_arithmetic<Property>::value || std::is_same<Property,std::string>::value) {
+            auto index = index_map_.find(field);
+            if(index == index_map_.end()) { return; }
+            property = boost::lexical_cast<Property>(elems_[index->second]);
+        } else {
+            std::string prefix = field + "/";
+            std::map<std::string, unsigned int> index_map;
+            for(auto kv: index_map_) {
+                if(!boost::starts_with(kv.first, prefix)) { continue; }
+                auto field = kv.first;
+                auto index = kv.second;
+                boost::algorithm::erase_first(field, prefix);
+                index_map[field] = index;
+            }
+            if(index_map.empty()) { return; }
+            auto visitor = Visitor(index_map);
+            visitor.set_elems(elems_);
+            Traits<Property>::visit(property, visitor);
+        }
     }
     void set_elems(std::vector<std::string> elems) { elems_ = elems; }
 private:
@@ -58,6 +78,7 @@ struct P {
 struct Q {
     std::string a;
     bool b;
+    P p;
     Q(): a("hello"), b(true) {}
 };
 
@@ -72,21 +93,19 @@ template<> struct Traits<Q> {
     static void visit(Q& q, const Visitor& visitor) {
         visitor.apply("a", q.a);
         visitor.apply("b", q.b);
+        visitor.apply("p", q.p);
     }
 };
 
-
+// fields "a,b,p/x,p/y"
 int main(int argc, char* argv[]) {
     std::string fields = argv[1];
-    // stream<P> stream(fields);
-    // while(std::cin) {
-    //     P p = stream.read();
-    //     std::cerr << std::setprecision(16) << p.x << " " << p.y << std::endl;
-    // }
     Stream<Q> stream(fields);
     while(std::cin) {
         Q q = stream.read();
-        std::cerr << q.a << " " << q.b << std::endl;
+        std::cerr << std::setprecision(16)
+            << " q.a=" << q.a << " q.b=" << q.b
+            << " q.p.x=" << q.p.x << " q.p.y=" << q.p.y << std::endl;
     }
 }
 
